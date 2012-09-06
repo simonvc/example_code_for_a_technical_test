1fe2b6586198bc82f630871e42f2c330ad6d0b27
Simon Vans-Colina <Simon@Vans-Colina.com>
2012-09-06 14:06:39 +0000

    added the jetty pre-reqs to the script including the JDK and the ANT

--- install_tools.sh
+++ install_tools.sh
@@ -9,11 +9,15 @@ EC2HOST=$1
 
 KEYFILE=$2
 
+ssh -i $KEYFILE ec2-user@$EC2HOST 'sudo yum -y update'
 ssh -i $KEYFILE ec2-user@$EC2HOST 'sudo yum -y install puppet git'
-ssh -i $KEYFILE ec2-user@$EC2HOST 'git clone https://github.com/simonvc/puppet-jetty.git'
+ssh -i $KEYFILE ec2-user@$EC2HOST 'git clone https://github.com/simonvc/puppet-jetty.git' # This is a fork of someones puppet-jetty manifest. Obviously we'd have our own one.
 ssh -i $KEYFILE ec2-user@$EC2HOST 'ls -ltr puppet-jetty'
-ssh -i $KEYFILE ec2-user@$EC2HOST 'cd puppet-jetty && sudo puppet apply manifests/init.pp'
 
+#pre-requisites
+ssh -i $KEYFILE ec2-user@$EC2HOST 'cd /etc/yum.repos.d&& sudo wget http://jpackage.org/jpackage.repo'
+ssh -i $KEYFILE ec2-user@$EC2HOST 'sudo yum install -y jdk ant'
 
-
+#Install jetty from our puppet repo
+ssh -i $KEYFILE ec2-user@$EC2HOST 'cd puppet-jetty && sudo puppet apply manifests/init.pp' # If instances are supposed to be long running, we'd use a puppet master and connect to that.
 
